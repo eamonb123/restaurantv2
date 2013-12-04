6a1bbe3d5f020c0c7e4320328ae49af5e2d582ce
Eamon Barkhordarian <ebarkhor@usc.edu>
2013-12-03 19:04:32 -0800

    updating restaurant

--- src/restaurant/gui/Gui.java
+++ src/restaurant/gui/Gui.java
@@ -2,10 +2,13 @@ package restaurant.gui;
 
 import java.awt.*;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 public interface Gui {
 
     public void updatePosition();
-    public void draw(Graphics2D g);
+    public void draw(Graphics2D g2);
     public boolean isPresent();
 
 }
